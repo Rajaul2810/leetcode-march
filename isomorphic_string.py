d151c5e68ae08621274f750c0d3e79cbd09f3476
unknown <rajaulkarim2810@gmail.com>
2023-03-14 17:38:23 +0600

    isomorphic_string

---
+++ isomorphic_string.py
@@ -0,0 +1,18 @@
+def isomorpic(s,t):
+    if(len(s)!= len(t)):
+        return False
+    mapping = {}
+    for i,j in zip(s,t):
+        mapping[i]=j
+    print(mapping)
+    if (len(set(mapping.values()))!= len(mapping.values())):
+        return False
+    newStr = ''
+    for i in s:
+        newStr += mapping[i]
+    if newStr == t:
+        return True
+    else:
+        return False
+
+P(isomorpic('egg','abb'))
